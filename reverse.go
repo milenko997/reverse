5205d9898169273c15c9e0d391ee32176dc01f16
milenko997 <48036062+milenko997@users.noreply.github.com>
2019-09-26 11:17:21 +0200

    rotate

--- reverse.go
+++ reverse.go
@@ -4,9 +4,8 @@ import "fmt"
 
 func main() {
 	ints := []int{1, 2, 3, 4, 5}
-	for i := 0; i < len(ints)/2; i++ {
-		end := len(ints) - i - 1
-		ints[i], ints[end] = ints[end], ints[i]
-	}
+	first := ints[0]
+	copy(ints, ints[1:])
+	ints[len(ints)-1] = first
 	fmt.Println(ints)
 }
